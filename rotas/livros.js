472196c30555c8491e50799e6b9072c4fb3fe41f
Vinicius Saboia <viniciuscsaboia@gmail.com>
2024-05-13 11:15:39 -0300

    first commit

---
+++ rotas/livros.js
@@ -0,0 +1,18 @@
+const { Router } = require('express')
+const { getLivros, getLivro, postLivro } = require('../controladores/livros')
+
+const router = Router()
+
+router.get('/', getLivros)
+
+router.get('/:id', getLivro)
+
+router.post('/', postLivro)
+router.patch('/', (req, res) => {
+    res.send('Você fez uma requisição do tipo PATCH')
+})
+router.delete('/', (req, res) => {
+    res.send('Você fez uma requisição do tipo DELETE')
+})
+
+module.exports = router
